b9837fd2afa0954c3f88226e861e7849baf8b6e9
Pranava Singhal <pranava227@gmail.com>
2025-12-10 19:47:52 -0800

    changed 2017 to 2016

--- tessera_ml/train_ssl.py
+++ tessera_ml/train_ssl.py
@@ -155,7 +155,7 @@ def main():
         index_dir=config["index_dir"],
         data_dir=config["data_root"],
         year=None,  # Random year from years list
-        years=list(range(2017, 2025)),  # Use years 2017-2024
+        years=list(range(2016, 2025)),  # Use years 2017-2024
         sample_size_s2=config["sample_size_s2"],
         sample_size_s1=config["sample_size_s1"],
         normalize=True,
